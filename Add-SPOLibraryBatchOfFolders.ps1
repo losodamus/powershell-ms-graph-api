d365bb93f5f61fe7718c385715bc78dadf12dc4a
Charles II <cjlakes@yahoo.com>
2022-08-06 21:40:09 -0700

    Add file via upload

---
+++ Add-SPOLibraryBatchOfFolders.ps1
@@ -0,0 +1,165 @@
+﻿<#
+    Add-SPOLibraryBatchOfFolders.ps1
+    ------------------------------
+
+
+    Batch create a list of folders 
+    in a target SharePoint Online 
+    document library. 
+#>
+cls
+
+
+##  Variable(s)
+$global:AppGuid = "<app_id>"
+$global:AppSecret = "<app_secret>"
+$global:TenantName = "<tenant_name>"
+
+
+[System.String] $sitePath = "/sites/Portal"
+[System.String] $librPath = "/Shared%20Documents"
+
+
+[System.Int32] $batchIndex = 0
+[System.Array] $batchOfFolders = @()
+[System.Array] $listOfFolders = @(
+    "Batch Folder 001", "Batch Folder 002", "Batch Folder 003", "Batch Folder 004", "Batch Folder 005",
+    "Batch Folder 006", "Batch Folder 007", "Batch Folder 008", "Batch Folder 009", "Batch Folder 010",
+    "Batch Folder 011", "Batch Folder 012", "Batch Folder 013", "Batch Folder 014", "Batch Folder 015",
+    "Batch Folder 016", "Batch Folder 017", "Batch Folder 018", "Batch Folder 019", "Batch Folder 020"
+)
+
+
+function Get-RestAPIHeader {
+    $Body = @{
+        client_id = "$($global:AppGuid)"
+	    client_secret = "$($global:AppSecret)"
+	    scope = "https://graph.microsoft.com/.default"
+	    grant_type = "client_credentials"
+    }
+
+
+    $PostSplat = @{
+        ContentType = "application/x-www-form-urlencoded"
+        Method = "POST"
+        Body = $Body
+        Uri = "https://login.microsoftonline.com/$($global:TenantName).onmicrosoft.com/oauth2/v2.0/token"
+    }
+    $Request = Invoke-RestMethod @PostSplat
+
+
+    return (@{
+        Authorization = "$($Request.token_type) $($Request.access_token)"
+    })
+}
+
+
+function Get-RestAPIResonse () {
+    param(
+        [Parameter(Mandatory)] 
+        [ValidateNotNullOrEmpty()] 
+        [ValidateSet('Get', 'Post', 'Patch')] 
+        [System.String] $APIMethod,
+
+
+        [Parameter(Mandatory)] 
+        [ValidateNotNullOrEmpty()] 
+        [ValidateSet('Beta', 'v1.0')] 
+        [System.String] $APIVersion,
+
+
+        [Parameter(Mandatory)] 
+        [ValidateNotNullOrEmpty()] 
+        [System.String] $APIResource
+    )
+
+
+    try {
+        $r = Invoke-RestMethod `
+            -Uri "https://graph.microsoft.com/$($APIVersion)/$($APIResource)" `
+            -Headers (Get-RestAPIHeader) `
+            -Method $APIMethod `
+            -ContentType "application/json"
+        return $r
+    }
+    catch [Exception] {
+        return $null
+    }
+}
+
+
+function Get-RestAPIBatchResponse {
+    param(
+        [Parameter(Mandatory)] 
+        [System.Array] $APIBatch
+    )
+
+
+    try {
+        $jsonBody = @{
+            "requests" = $APIBatch
+        } | ConvertTo-Json -Depth 5
+        $jsonBody | Out-Host
+
+
+        $r = (Invoke-RestMethod `
+            -Uri "https://graph.microsoft.com/v1.0/`$batch" `
+            -Headers (Get-RestAPIHeader) `
+            -Body $jsonBody `
+            -Method POST `
+            -ContentType "application/json")
+        return $r
+    }
+    catch [Exception] {
+        return $null
+    }
+}
+cls
+
+
+try {
+    ##  E.g., <tenant_name>.sharepoint.com,076bf298-a1b2-49b8-9658-9c9796e3623c,7e80afae-d0f6-abcd-1234-773b9fe11381
+    ##  --------------------------------------------------
+    foreach($site in (Get-RestAPIResonse `
+                        -APIMethod Get `
+                        -APIVersion v1.0 `
+                        -APIResource "sites/$($global:TenantName).sharepoint.com:/$($sitePath)/?`$select=id,name,webUrl")) {
+
+
+        ##  E.g., b!mPJrB6couEmWWJyXluNiPK6vgH720IpBh_R404_hE4H0-fEDWHMKQ6oR_pSl_atl
+        ##  --------------------------------------------------
+        foreach($libr in (Get-RestAPIResonse `
+                            -APIMethod Get `
+                            -APIVersion v1.0 `
+                            -APIResource "sites/$($site.id)/drives/?`$select=id,name,webUrl").value | ? { $_.webUrl -like "*$($librPath)" }) {
+
+
+            ##  Enumerate folder array
+            foreach($folder in $listOfFolders) {
+
+
+                $batchIndex += 1
+                $batchOfFolders += @{
+                    "url" = "/sites/$($site.id)/drives/$($libr.id)/root/children"
+                    "method" = "POST"
+                    "id" = "$($batchIndex)"
+                    "body" = @{
+                        "name" = "$($folder)"
+                        "folder" = @{}
+                        "@microsoft.graph.conflictBehavior" = "rename"
+                    }
+                    "headers" = @{
+                        "Content-Type" = "application/json"
+                    }
+                }
+            }
+
+
+            Get-RestAPIBatchResponse `
+                -APIBatch $batchOfFolders
+        }
+    }
+}
+catch [Exception] {
+    Write-Error $_.Exception.Message
+}
