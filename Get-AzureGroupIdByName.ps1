0b9bf82ddcff4e13d63b234b687d23448cae2a69
Charles II <cjlakes@yahoo.com>
2022-09-09 18:17:27 -0700

    Add file via upload
    Initial upload of script.

---
+++ Get-AzureGroupIdByName.ps1
@@ -0,0 +1,125 @@
+﻿<#
+    Get-AzureGroupIdByName.ps1
+#>
+
+
+##  Variable(s)
+$global:AppGuid = "<app_id>"
+$global:AppSecret = "<app_secret>"
+$global:TenantName = "<tenant_name>"
+[System.String] $groupName = "<group_name>"
+
+
+function Get-RestAPIHeader {
+    $Body = @{
+        client_id = "$($global:AppGuid)"
+	    client_secret = "$($global:AppSecret)"
+	    scope = "https://graph.microsoft.com/.default"
+	    grant_type = "client_credentials"
+    }
+
+
+    $PostSplat = @{
+        ContentType = "application/x-www-form-urlencoded"
+        Method = "POST"
+        Body = $Body
+        Uri = "https://login.microsoftonline.com/$($global:TenantName).onmicrosoft.com/oauth2/v2.0/token"
+    }
+    $Request = Invoke-RestMethod @PostSplat
+
+
+    return (@{
+        Authorization = "$($Request.token_type) $($Request.access_token)"
+    })
+}
+
+
+function Get-RestAPIResonse () {
+    param(
+        [Parameter(Mandatory)] 
+        [ValidateNotNullOrEmpty()] 
+        [ValidateSet('Get', 'Post', 'Patch')] 
+        [System.String] $APIMethod,
+
+
+        [Parameter(Mandatory)] 
+        [ValidateNotNullOrEmpty()] 
+        [ValidateSet('Beta', 'v1.0')] 
+        [System.String] $APIVersion,
+
+
+        [Parameter(Mandatory)] 
+        [ValidateNotNullOrEmpty()] 
+        [System.String] $APIResource
+    )
+
+
+    try {
+        $r = Invoke-RestMethod `
+            -Uri "https://graph.microsoft.com/$($APIVersion)/$($APIResource)" `
+            -Headers (Get-RestAPIHeader) `
+            -Method $APIMethod `
+            -ContentType "application/json"
+        return $r
+    }
+    catch [Exception] {
+        return $null
+    }
+}
+
+
+function Get-RestAPIBatchResponse {
+    param(
+        [Parameter(Mandatory)] 
+        [System.Array] $APIBatch
+    )
+
+
+    try {
+        $jsonBody = @{
+            "requests" = $APIBatch
+        } | ConvertTo-Json -Depth 5
+        $jsonBody | Out-Host
+
+
+        $r = (Invoke-RestMethod `
+            -Uri "https://graph.microsoft.com/v1.0/`$batch" `
+            -Headers (Get-RestAPIHeader) `
+            -Body $jsonBody `
+            -Method POST `
+            -ContentType "application/json")
+        return $r
+    }
+    catch [Exception] {
+        return $null
+    }
+}
+
+
+##  E.g., 8317d5d7-84a4-a404-bb00-1f865ae63203
+##  --------------------------------------------------
+function Get-GroupIdByName {
+    param(
+        [Parameter(Mandatory)] 
+        [System.String] $NameOfGroup
+    )
+
+
+    [System.String] $t = ""
+    foreach($g in (Get-RestAPIResonse `
+                    -APIMethod Get `
+                    -APIVersion v1.0 `
+                    -APIResource "groups/?`$select=id,displayName&`$filter=startswith(displayName,'$($NameOfGroup)')").value) {
+        $t = "$($g.id)"
+    }
+    return $t
+}
+cls
+
+
+try {
+    Get-GroupIdByName -NameOfGroup $groupName
+}
+catch [Exception] {
+    Write-Error $_.Exception.Message
+}
